a7e053352aaa7aa50d67e787692b85eaacf27022
Robert Yacob <yacobrobert@hotmail.com>
2019-10-04 16:54:04 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-      <h1>Hello man</h1>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      <Navbar />
     </div>
   );
 }
