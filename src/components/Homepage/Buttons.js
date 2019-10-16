2f6903d719ab36a7a1baabf78de57c86f977f05f
Robert Yacob <yacobrobert@hotmail.com>
2019-10-16 10:30:12 +0200

    little change 3

--- src/components/Homepage/Buttons.js
+++ src/components/Homepage/Buttons.js
@@ -3,33 +3,42 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined';
 import GpsFixedOutlinedIcon from '@material-ui/icons/GpsFixedOutlined';
-import { Box } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 
 const buttons = (props) => {
     const styles = makeStyles(theme => ({
+        root: {
+            flexGrow: 1,
+        },
         button: {
-            margin: theme.spacing(2),
+            margin: theme.spacing(1),
         }
     }));
 
     const classes = styles();
 
     return (
-        <div>
-            <Box display="flex" justifyContent="center" >
-                <Button
-                    className={classes.button}
-                    variant="contained"
-                    color="secondary"
-                    startIcon={<AddBoxOutlinedIcon />}>Sell a new object
-                </Button>
-                <Button
-                    className={classes.button}
-                    variant="contained"
-                    color="primary"
-                    startIcon={<GpsFixedOutlinedIcon />}>Find around me !
-                </Button>           
-            </Box>
+        <div className={classes.root}>
+            <Grid container direction="row" justify="center" alignItems="center" spacing={2}>
+                <Grid item xs={11} sm={5} align="center">
+                    <Button
+                        className={classes.button}
+                        variant="contained"
+                        color="secondary"
+                        fullWidth={true}
+                        startIcon={<AddBoxOutlinedIcon />}>Sell a new object
+                    </Button>
+                </Grid>
+                <Grid item xs={11} sm={5} align="center">
+                    <Button
+                        className={classes.button}
+                        variant="contained"
+                        color="primary"
+                        fullWidth={true}
+                        startIcon={<GpsFixedOutlinedIcon />}>Find around me
+                    </Button>                      
+                </Grid>
+            </Grid>         
         </div>
     );
 };
