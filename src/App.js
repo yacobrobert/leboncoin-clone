6b1d3edbaf78241d7aba641994ffbb8297573de2
Robert Yacob <yacobrobert@hotmail.com>
2019-11-06 16:02:33 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout';
 import { ThemeProvider } from '@material-ui/styles';
@@ -14,13 +15,15 @@ const theme=createMuiTheme({
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:600,400,500,700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        <Layout />
-      </div>
-    </ThemeProvider>    
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <div>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:600,400,500,700&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          <Layout />
+        </div>
+      </ThemeProvider> 
+    </BrowserRouter>   
   );
 };
 
