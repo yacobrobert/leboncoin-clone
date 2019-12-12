86cbe3be26bad8fd85e3227466de9879bb0f8161
Robert Yacob <yacobrobert@hotmail.com>
2019-12-12 13:31:28 +0100

    last 3

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -13,27 +13,31 @@ const homepage = (props) => {
     const styles = makeStyles(theme => ({
         root: {
             padding: theme.spacing(1, 1),
-            margin: theme.spacing(1),
+            margin: theme.spacing(1, 1),
             flexGrow: 1, 
+            spacing: 2
         },
     }));
 
     const classes = styles();
     
     return (
-        <div className={classes.root}>               
-            <Grid container alignItems="center" justify="center" spacing={2}  >
-                <Grid>
-                    <Switch>
-                        <Route path="/weapons" component={Weapons}></Route>
-                        <Route path="/login" component={Login}></Route>
-                        <Route path="/map" component={GoogleApiWrapper}></Route>
-                        <Route path="/" exact component={CardHomePage}></Route>
-                    </Switch>
-                </Grid>
-            </Grid>                  
-            <Buttons />          
-        </div>
+        <React.Fragment>
+            <div className={classes.root}>               
+                <Grid container alignItems="center" justify="center">
+                    <Grid>
+                        <Switch>
+                            <Route path="/weapons" component={Weapons}></Route>
+                            <Route path="/login" component={Login}></Route>
+                            <Route path="/map" component={GoogleApiWrapper}></Route>
+                            <Route path="/" exact component={CardHomePage}></Route>
+                        </Switch>
+                    </Grid>
+                </Grid>              
+            </div>                   
+            <Buttons />   
+        </React.Fragment>
+
     );    
 };
 
